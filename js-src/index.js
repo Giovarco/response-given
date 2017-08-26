d968c30ff3b647c058d33682ff4a9649290216a0
Unknown <kirby94@hotmail.it>
2017-08-26 23:24:40 +0200

    - Try to import one js library

--- js-src/index.js
+++ js-src/index.js
@@ -1,6 +1,23 @@
 "use strict";
+/*
+INPUT: JSON = CONFIGURATION TO OBTAIN MESSAGE RESPONSE
+OUTPUT: EMIT EVENTS
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
-function greet() {
-    console.log("HELLO WORLD!");
+// Imports
+/// <reference path='../node_modules/node-json-equal/index.d.ts' />
+var nodeJsonEqual = require("node-json-equal");
+// Configuration
+var messages = {};
+// Public functions
+function setMessages(_messages) {
+    console.log(isEmpty(_messages));
 }
-exports.greet = greet;
+exports.setMessages = setMessages;
+// Private functions
+function isEmpty(_messages) {
+    var options = { arrayOrder: false };
+    console.log(nodeJsonEqual.equal(_messages, {}, options));
+    return nodeJsonEqual.equal(_messages, {}, options);
+}
+setMessages({});
--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,3 +1,25 @@
-export function greet() {
-    console.log("HELLO WORLD!");
+/*
+INPUT: JSON = CONFIGURATION TO OBTAIN MESSAGE RESPONSE
+OUTPUT: EMIT EVENTS
+*/
+
+// Imports
+/// <reference path='../node_modules/node-json-equal/index.d.ts' />
+import nodeJsonEqual = require("node-json-equal");
+
+// Configuration
+let messages : Object = {}
+
+// Public functions
+export function setMessages(_messages : Object) : void {
+    console.log(isEmpty(_messages))
 }
+
+// Private functions
+function isEmpty(_messages : Object) : boolean {
+    const options : Object = {arrayOrder : false};
+    console.log(nodeJsonEqual.equal(_messages, {}, options));
+    return nodeJsonEqual.equal(_messages, {}, options);
+}
+
+setMessages({});
