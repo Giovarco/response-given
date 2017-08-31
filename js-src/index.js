a97a87a27cf08384d997ea3d67add8eaae10e8aa
Unknown <kirby94@hotmail.it>
2017-08-31 22:54:55 +0200

    - Use Winston to log - Upload docker related files

--- js-src/index.js
+++ js-src/index.js
@@ -3,17 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Imports and globals
 var typescript_events_1 = require("typescript.events");
 var eventEmitter = new typescript_events_1.Event();
+var winston = require("winston");
+var logger = new (winston.Logger)({
+    transports: [
+        // colorize the output to the console
+        new (winston.transports.Console)({ colorize: true })
+    ]
+});
+logger.level = 'debug';
 // Internal state
 var dictionary = {};
 // Public functions
 function setDictionary(_dictionary) {
     if (!isEmpty(_dictionary) && hasCorrectFormat(_dictionary)) {
         dictionary = _dictionary;
-        console.log("OK");
+        logger.debug("OK");
     }
     else {
         eventEmitter.emit("error", new Error("The input dictionary cannot be an empty JSON"));
-        console.log("ERROR");
+        logger.debug("ERROR");
     }
 }
 exports.setDictionary = setDictionary;
--- ts-src/index.ts
+++ ts-src/index.ts
@@ -2,17 +2,27 @@
 import {Event} from "typescript.events";
 let eventEmitter = new Event();
 
+import * as winston from 'winston';
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({ colorize: true })
+  ]
+});
+logger.level = 'debug';
+
+
 // Internal state
-let dictionary : Object = {}
+let dictionary : object = {}
 
 // Public functions
-export function setDictionary(_dictionary : Object) : void {
+export function setDictionary(_dictionary : object) : void {
     if(!isEmpty(_dictionary) && hasCorrectFormat(_dictionary)) {
       dictionary = _dictionary;
-      console.log("OK");
+      logger.debug("OK");
     } else {
       eventEmitter.emit("error", new Error("The input dictionary cannot be an empty JSON"));
-      console.log("ERROR")
+      logger.debug("ERROR");
     }
 }
 
@@ -21,7 +31,7 @@ export function getMessage() {
 }
 
 // Private functions
-function isEmpty(obj : Object) : boolean {
+function isEmpty(obj : object) : boolean {
 
     for (let key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
@@ -33,7 +43,7 @@ function isEmpty(obj : Object) : boolean {
 
 }
 
-function hasCorrectFormat(_dictionary : Object) : boolean {
+function hasCorrectFormat(_dictionary : object) : boolean {
   return true;
 }
 
